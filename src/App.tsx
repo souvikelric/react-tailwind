af5838941166e9c758dec52b9783bfe5d3f5fb7a
Souvik <souvikelric@gmail.com>
2025-03-08 19:11:06 +0530

    added a few changes to styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,12 @@ import pic5 from "./pics/5.jpg";
 function App() {
   return (
     <div className="px-20 pt-5 min-h-screen bg-gray-800">
-      <h1 className="text-7xl text-blue-400 ">Vite + React</h1>
+      <h1 className="text-7xl text-blue-400 ">React Expense Tracker</h1>
       <p className="text-white my-10 w-10/12 text-2xl">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        exercitationem odio modi ut eius ab deleniti enim expedita praesentium
-        laudantium, tempora perspiciatis culpa earum rem illo, vitae ex
-        temporibus iste inventore nobis quae maiores doloribus porro adipisci?
-        Magnam voluptatibus debitis mollitia exercitationem adipisci atque ea.
+        Effortless Budgeting, Real-time Tracking, Smarter Spending. Welcome to
+        🔥React Expense Tracker🔥 — your personal finance companion. Whether
+        you're saving for a dream vacation, managing monthly budgets, or simply
+        keeping tabs on daily expenses, we've got you covered.
       </p>
       <div className="flex gap-6">
         <div>
@@ -52,7 +51,16 @@ function App() {
           />
         </div>
       </div>
-      <InfoCards />
+      <div className="flex justify-center gap-5">
+        <InfoCards
+          header="Monthly Expense"
+          paragraph="Calculate and keep a track of your monthly expenses. Add, remove and calculate your monthly expenses"
+        />
+        <InfoCards
+          header="Weekly Expense"
+          paragraph="Calculate and keep a track of your weekly expenses. Add, remove and calculate your weekly expenses"
+        />
+      </div>
       <div className="h-3"></div>
     </div>
   );
--- src/components/InfoCards/InfoCards.tsx
+++ src/components/InfoCards/InfoCards.tsx
@@ -1,12 +1,15 @@
-export default function InfoCards() {
+interface InfoCardProp {
+  header: string;
+  paragraph: string;
+}
+export default function InfoCards({ header, paragraph }: InfoCardProp) {
   return (
-    <div className="h-1/2 w-90 flex flex-col gap-3 bg-purple-950 text-white mt-10 p-5">
-      <div className="text-4xl">Card Header 1</div>
-      <div className="text-[16px]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        officia culpa sunt nostrum dicta iure repudiandae minima nihil quod
-        voluptate?
-      </div>
+    <div
+      className="h-50 w-170 flex flex-col gap-3 bg-gray-900 text-white mt-10 p-5
+    shadow-xs cursor-pointer"
+    >
+      <div className="text-4xl text-amber-200">{header}</div>
+      <div className="text-[26px]">{paragraph}</div>
     </div>
   );
 }
